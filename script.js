446948e79eac92df7a0c85362baa4133c3b3507c
Anvi1212Agarwal <167762387+Anvi1212Agarwal@users.noreply.github.com>
2024-11-12 17:08:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+var timer = 60;
+var score =0;
+var hitrn = 0;
+
+function increaseScore(){
+     score += 10;
+     document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+function MakeBuuble(){
+    var clutter = "";
+
+for(var i = 1; i<=126; i++){
+   var rn = Math.floor(Math.random()*10)
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbottom").innerHTML = clutter;
+
+}
+function runTimer(){
+    var timerint = setInterval(function(){
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        } else {
+            clearInterval(timerint);
+            document.querySelector("#pbottom").innerHTML = `<h1>Game Over</h1>`;
+        }
+    }, 1000);
+}
+
+document.querySelector("#pbottom")
+.addEventListener("click",function(dets){
+   var clikednum = Number(dets.target.textContent);
+   if(clikednum === hitrn){
+    increaseScore();
+    MakeBuuble();
+    getNewHit();
+   }
+})
+
+runTimer();
+MakeBuuble();
+getNewHit();
